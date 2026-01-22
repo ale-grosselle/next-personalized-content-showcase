228418c84fedada7d773a36205b01340d35ea598
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2026-01-22 09:45:17 +0100

    chore: remove async

--- app/(pages)/with-use-cache/page.tsx
+++ app/(pages)/with-use-cache/page.tsx
@@ -1,16 +1,16 @@
-import {BannerWithUseCache} from './staticComponent/Banner.static';
-import {RecommendedForYou} from "@/app/components/RecommendedForYou";
+import { BannerWithUseCache } from './staticComponent/Banner.static';
+import { RecommendedForYou } from '@/app/components/RecommendedForYou';
 
-export default async function HomePage() {
-    console.log('Hello from Homepage app/page-with-use-cache/page.tsx');
-    return (
-        <>
-            <RecommendedForYou
-                title="Recommended for you"
-                subtitle="Products selected based on your preferences"
-                limit={8}
-            />
-            <BannerWithUseCache/>
-        </>
-    );
+export default function HomePage() {
+  console.log('Hello from Homepage app/page-with-use-cache/page.tsx');
+  return (
+    <>
+      <RecommendedForYou
+        title="Recommended for you"
+        subtitle="Products selected based on your preferences"
+        limit={8}
+      />
+      <BannerWithUseCache />
+    </>
+  );
 }
